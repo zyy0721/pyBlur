98b458bbc5a629d3d7234e2c1a3e3174e5346651
zyy0721 <zhangyy_0721@163.com>
2019-12-16 15:58:18 +0800

    add main framework of pyBlur

--- pyBlur.py
+++ pyBlur.py
@@ -9,3 +9,24 @@ from ast import Assign, Name, Call, Store, Load, Str, Num, List, Add, BinOp
 from ast import Subscript, Slice, Attribute, GeneratorExp, comprehension
 from ast import Compare, Mult
 
+def string_random(minlen, maxlen):
+    return xxx
+
+def import_random(name, rdname):
+    return xxx
+
+def string_blur(str):
+    return xxx
+
+class Blur(ast.NodeTransformer):
+
+
+class GlobalVarBlur(ast.NodeTransformer):
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: python %s <your pyfile>' % sys.argv[0])
+        exit(0)
+
+    
