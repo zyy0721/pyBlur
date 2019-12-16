6b4d03a675381b0ef9d6eb2ec85e4b477ef3b030
zyy0721 <zhangyy_0721@163.com>
2019-12-16 19:24:37 +0800

    add more visit function for pyBlur

--- pyBlur.py
+++ pyBlur.py
@@ -19,9 +19,41 @@ def string_blur(str):
     return xxx
 
 class Blur(ast.NodeTransformer):
+    def __init__(self):
+        ast.NodeTransformer.__init__(self)
+        #imported modules
 
+        #global values
+
+        #local values
+
+        #inside a function
+
+    def Blur_global(self, name):
+    
+    def Blur_local(self, name):
+
+    def visit_Import(self, node):
+
+    def visit_If(self, node):
+
+    def visit_Str(self, node):
+
+    def visit_Num(self, node):
+
+    def visit_Attribute(self, node):
+
+    def visit_FunctionDef(self, node):
+
+    def visit_Name(self, node):
+
+    def visit_Module(self, node):
 
 class GlobalVarBlur(ast.NodeTransformer):
+    def __init__(selff, globals):
+
+    def visit_Name(self, node):
+        
 
 
 if __name__ == "__main__":
