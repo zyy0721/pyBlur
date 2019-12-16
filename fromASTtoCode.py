b39ff410d4aaaa027d219e3ea0dd05069484cbad
zyy0721 <zhangyy_0721@163.com>
2019-12-16 16:28:14 +0800

    add main frame work of fromASTtoCode

--- fromASTtoCode.py
+++ fromASTtoCode.py
@@ -1,1 +1,20 @@
 from ast import *
+
+#BINOP SYMBOLS
+
+#UNARYOP SYMBOLS
+
+#BOOLOP SYMBOLS
+
+#CMPOP SYMBOLS
+
+class fromASTtoCode(NodeVisitor):
+
+
+
+
+def AST2Code(node, indentation = ' ' * 4, flagLineInfo = False):
+    code = fromASTtoCode(indentation, flagLineInfo)
+    code.visit(node)
+
+    return ''.join(code.result)
